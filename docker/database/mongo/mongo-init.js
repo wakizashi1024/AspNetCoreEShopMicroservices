7913f812a03f92dbaaa1bda1ac1fb0a889bf3308
Wakizashi1024 <wakizashi1024@outlook.com>
2024-03-30 17:24:11 +0800

    Add Docker settings: Mongo

---
+++ docker/database/mongo/mongo-init.js
@@ -0,0 +1,33 @@
+﻿db.createUser({
+    user: "user", 
+    pwd: "user1234",
+    roles: [
+        {
+            role: "readWrite",
+            db: "CatalogDb",
+        },
+    ],
+});
+
+db.createCollection('Products');
+
+db.Products.insertMany([
+    {
+        "Name": "Asus Laptop",
+        "Category": "Computers",
+        "Summary": "Summary",
+        "Description": "Description",
+        "ImageFile": "ImageFile",
+        "Price": 54.93
+    },
+    {
+        "Name": "HP Laptop",
+        "Category": "Computers",
+        "Summary": "Summary",
+        "Description": "Description",
+        "ImageFile": "ImageFile",
+        "Price": 88.93
+    }
+]);
+
+db.Products.remove({});
