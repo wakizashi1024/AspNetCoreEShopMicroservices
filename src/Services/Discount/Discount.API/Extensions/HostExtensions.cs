7214bb21b24d9e096770fb781f8f54bcea6994b5
Wakizashi1024 <wakizashi1024@outlook.com>
2024-04-02 23:38:48 +0800

    Discount.API: Refactor HostExtensions MigrateDatabase retry with Polly

--- src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -1,13 +1,12 @@
 ﻿using Npgsql;
+using Polly;
 
 namespace Discount.API.Extensions;
 
 public static class HostExtensions
 {
-    public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+    public static IHost MigrateDatabase<TContext>(this IHost host)
     {
-        int retryForAvailability = retry.Value;
-
         using (var scope = host.Services.CreateScope())
         {
             var services = scope.ServiceProvider;
@@ -18,67 +17,26 @@ public static class HostExtensions
             {
                 logger.LogInformation("Migration postgres database");
 
-                using var connection =
-                    new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-                connection.Open();
+                var retry = Policy.Handle<NpgsqlException>()
+                    .WaitAndRetry(
+                        retryCount: 5,
+                        sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                        onRetry: (exception, retryCount, context) =>
+                        {
+                            logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to {exception}.");
+                        }
+                    );
 
-                using var command = new NpgsqlCommand
-                {
-                    Connection = connection,
-                };
-
-                var resetCouponTable = configuration.GetValue<bool>("DatabaseSettings:ResetCouponTable");
-                if (resetCouponTable)
-                {
-                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
-                    command.ExecuteNonQuery();
-                }
-                
-                command.CommandText = @"CREATE TABLE IF NOT EXISTS Coupon (
-	                                        ID SERIAL PRIMARY KEY NOT NULL,
-	                                        ProductName VARCHAR(24) NOT NULL,
-	                                        Description TEXT,
-	                                        Amount INT
-                                        );";
-                command.ExecuteNonQuery();
-                
-                command.CommandText = @"INSERT INTO Coupon (ProductName, Description, Amount)
-                                        SELECT 'IPhone X', 'IPhone Discount', 150
-                                        WHERE NOT EXISTS (
-	                                        SELECT 1
-	                                        FROM Coupon
-	                                        WHERE ProductName = 'IPhone X' 
-		                                        AND Description = 'IPhone Discount'
-	                                            AND Amount = 150
-                                        );";
-                command.ExecuteNonQuery();
-                
-                command.CommandText = @"INSERT INTO Coupon (ProductName, Description, Amount)
-                                        SELECT 'Samsung 10', 'Samsung Discount', 100
-                                        WHERE NOT EXISTS (
-	                                        SELECT 1
-	                                        FROM Coupon
-	                                        WHERE ProductName = 'Samsung 10'
-		                                        AND Description = 'Samsung Discount'
-	                                            AND Amount = 100
-                                        );";
-                command.ExecuteNonQuery();
+                retry.Execute(() => ExecuteMigrations((configuration)));
                 
                 logger.LogInformation("Migration postgres database done");
             }
             catch (NpgsqlException e)
             {
                 logger.LogError(
-                        e, 
+                        e,
                         "An error occurred while migrating the postresql database"
                     );
-
-                if (retryForAvailability < 50)
-                {
-                    retryForAvailability++;
-                    Thread.Sleep(2000);
-                    MigrateDatabase<TContext>(host, retryForAvailability);
-                }
             }
             catch (Exception e)
             {
@@ -91,5 +49,55 @@ public static class HostExtensions
         }
 
         return host;
+    }
+
+    private static void ExecuteMigrations(IConfiguration configuration)
+    {
+        using var connection = new NpgsqlConnection(
+            configuration.GetValue<string>("DatabaseSettings:ConnectionString")
+            );
+        connection.Open();
+
+        using var command = new NpgsqlCommand
+        {
+            Connection = connection,
+        };
+
+        var resetCouponTable = configuration.GetValue<bool>("DatabaseSettings:ResetCouponTable");
+        if (resetCouponTable)
+        {
+            command.CommandText = "DROP TABLE IF EXISTS Coupon";
+            command.ExecuteNonQuery();
+        }
+        
+        command.CommandText = @"CREATE TABLE IF NOT EXISTS Coupon (
+	                                ID SERIAL PRIMARY KEY NOT NULL,
+	                                ProductName VARCHAR(24) NOT NULL,
+	                                Description TEXT,
+	                                Amount INT
+                                );";
+        command.ExecuteNonQuery();
+        
+        command.CommandText = @"INSERT INTO Coupon (ProductName, Description, Amount)
+                                SELECT 'IPhone X', 'IPhone Discount', 150
+                                WHERE NOT EXISTS (
+	                                SELECT 1
+	                                FROM Coupon
+	                                WHERE ProductName = 'IPhone X' 
+		                                AND Description = 'IPhone Discount'
+	                                    AND Amount = 150
+                                );";
+        command.ExecuteNonQuery();
+        
+        command.CommandText = @"INSERT INTO Coupon (ProductName, Description, Amount)
+                                SELECT 'Samsung 10', 'Samsung Discount', 100
+                                WHERE NOT EXISTS (
+	                                SELECT 1
+	                                FROM Coupon
+	                                WHERE ProductName = 'Samsung 10'
+		                                AND Description = 'Samsung Discount'
+	                                    AND Amount = 100
+                                );";
+        command.ExecuteNonQuery();
     }
 }
